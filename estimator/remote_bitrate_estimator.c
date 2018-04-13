c634287010f331491ddbf149047dff4f04ef10e2
rongxi.yuan <rongxi.yuan@wenba100.com>
2018-04-13 18:17:52 +0800

    增加接收端的kalman filter带宽评估机制

---
+++ estimator/remote_bitrate_estimator.c
@@ -0,0 +1,171 @@
+#include "remote_bitrate_estimator.h"
+
+#define k_min_bitrate					64000	  /*8KB/S*/
+#define k_max_bitrate					12800000  /*1.6MB/S*/
+#define k_process_interval_ms			500
+
+static void rbe_update_estimate(remote_bitrate_estimator_t* est, int64_t now_ts);
+
+remote_bitrate_estimator_t* rbe_create()
+{
+	remote_bitrate_estimator_t* rbe = calloc(1, sizeof(remote_bitrate_estimator_t));
+	rbe->interval_ts = k_process_interval_ms;
+	rbe->last_update_ts = GET_SYS_MS();
+	rbe->last_packet_ts = -1;
+
+	/*初始化带宽统计器*/
+	rbe->last_incoming_bitrate = -1;
+	rate_stat_init(&rbe->incoming_bitrate, 1000, 8000);
+
+	rbe->aimd = aimd_create(k_min_bitrate, k_max_bitrate);
+	rbe->detector = overuse_create();
+	rbe->inter_arrival = create_inter_arrival(0, 5);
+	rbe->kalman = kalman_filter_create();
+
+	return rbe;
+}
+
+void rbe_destroy(remote_bitrate_estimator_t* est)
+{
+	if (est == NULL)
+		return;
+
+	rate_stat_destroy(&est->incoming_bitrate);
+
+	if (est->aimd != NULL){
+		aimd_destroy(est->aimd);
+		est->aimd = NULL;
+	}
+
+	if (est->detector != NULL){
+		overuse_create(est->detector);
+		est->detector = NULL;
+	}
+
+	if (est->inter_arrival != NULL){
+		destroy_inter_arrival(est->inter_arrival);
+		est->inter_arrival = NULL;
+	}
+
+	if (est->kalman != NULL){
+		kalman_filter_destroy(est->kalman);
+		est->kalman = NULL;
+	}
+
+	free(est);
+}
+
+static void rbe_reset(remote_bitrate_estimator_t* est)
+{
+	if (est->detector != NULL){
+		overuse_destroy(est->detector);
+		est->detector = overuse_create();
+	}
+
+	if (est->inter_arrival != NULL){
+		destroy_inter_arrival(est->inter_arrival);
+		est->inter_arrival = create_inter_arrival(0, 5);
+	}
+
+	if (est->kalman != NULL){
+		kalman_filter_destroy(est->kalman);
+		est->kalman = kalman_filter_create();
+	}
+}
+
+void rbe_update_rtt(remote_bitrate_estimator_t* est, uint32_t rtt)
+{
+	aimd_set_rtt(est->aimd, rtt);
+}
+
+void rbe_set_min_bitrate(remote_bitrate_estimator_t* est, uint32_t min_bitrate)
+{
+	aimd_set_min_bitrate(est->aimd, min_bitrate);
+}
+
+int rbe_last_estimate(remote_bitrate_estimator_t* est, uint32_t* bitrate_bps)
+{
+	if (est->aimd->inited == -1)
+		return -1;
+
+	*bitrate_bps = est->aimd->curr_rate;
+
+	return 0;
+}
+
+void rbe_heartbeat(remote_bitrate_estimator_t* est, int64_t now_ts)
+{
+	if (now_ts >= est->last_update_ts + est->interval_ts){
+		est->last_update_ts = now_ts;
+		rbe_update_estimate(est, now_ts);
+
+		/*进行REMB的发送*/
+	}
+}
+
+void rbe_incoming_packet(remote_bitrate_estimator_t* est, uint32_t timestamp, int64_t arrival_ts, size_t payload_size)
+{
+	int64_t now_ts, delta_arrival;
+	uint32_t delta_ts;
+
+	int incoming_rate, prev_state, delta_size;
+
+	now_ts = GET_SYS_MS();
+
+	/*就算当前接收到数据的带宽码率*/
+	incoming_rate = rate_stat_rate(&est->incoming_bitrate, now_ts);
+	if (incoming_rate >= 0){
+		est->last_incoming_bitrate = incoming_rate;
+	}
+	else if (est->last_incoming_bitrate > 0){
+		rate_stat_reset(&est->incoming_bitrate);
+		est->last_incoming_bitrate = 0;
+	}
+	rate_stat_update(&est->incoming_bitrate, payload_size, now_ts);
+
+	rbe->last_packet_ts = now_ts;
+
+	prev_state = est->detector->state;
+	delta_arrival = 0;
+	delta_ts = 0;
+	delta_size = 0;
+
+	/*进行kalman filter判断网络是否过载*/
+	if (inter_arrival_compute_deltas(est->inter_arrival, timestamp, arrival_ts, now_ts, payload_size, &delta_ts, &delta_arrival, &delta_size) == 0){
+		kalman_filter_update(est->kalman, delta_arrival, delta_ts, delta_size, est->detector->state, now_ts);
+		overuse_detect(est->detector, est->kalman->offset, delta_ts, est->kalman->num_of_deltas, now_ts);
+	}
+
+	/*网络过载了*/
+	if (est->detector->state == kBwOverusing){
+		incoming_rate = rate_stat_rate(&est->incoming_bitrate, now_ts);
+		/*网络的过载状态发生变化了，必须立即下调码率*/
+		if (incoming_rate > 0 && (prev_state != kBwOverusing || aimd_time_reduce_further(est->aimd, now_ts, incoming_rate) == 0))
+			rbe_update_estimate(est, now_ts);
+	}
+}
+
+static void rbe_update_estimate(remote_bitrate_estimator_t* est, int64_t now_ts)
+{
+	int state;
+	double sum_var_noise = 0.0;
+	rate_control_input_t input;
+	uint32_t target_bitrate;
+
+	state = kBwNormal;
+	if (now_ts > est->last_packet_ts + 2000){
+		rbe_reset(est);
+	}
+	else{
+		sum_var_noise = est->kalman->var_noise;
+		state = est->detector->state;
+	}
+
+	input.noise_var = sum_var_noise;
+	input.incoming_bitrate = rate_stat_rate(&est->incoming_bitrate, now_ts);
+	input.state = state;
+	target_bitrate = aimd_update(est->aimd, &input, now_ts);
+	if (est->aimd->inited == 0)
+		est->interval_ts = 300;
+}
+
---
+++ estimator/remote_bitrate_estimator.h
@@ -0,0 +1,42 @@
+#ifndef __remote_bitrate_estimator_h_
+#define __remote_bitrate_estimator_h_
+
+#include "aimd_rate_control.h"
+#include "inter_arrival.h"
+#include "overuse_detector.h"
+#include "kalman_filter.h"
+#include "rate_stat.h"
+
+/*接收端利用卡尔曼算法和aimd来评估发送端带宽，然后通过REMB方式反馈给发送端进行带宽调节,这里只是做一个可选测试*/
+typedef struct
+{
+	int64_t						last_update_ts;
+	int64_t						interval_ts;
+	int64_t						last_packet_ts;
+
+	int							last_incoming_bitrate;
+	rate_stat_t					incoming_bitrate;
+
+	aimd_rate_controller_t*		aimd;
+	overuse_detector_t*			detector;
+	inter_arrival_t*			inter_arrival;
+	kalman_filter_t*			kalman;
+}remote_bitrate_estimator_t;
+
+remote_bitrate_estimator_t*		rbe_create();
+void							rbe_destroy(remote_bitrate_estimator_t* est);
+
+void							rbe_update_rtt(remote_bitrate_estimator_t* est, uint32_t rtt);
+void							rbe_set_min_bitrate(remote_bitrate_estimator_t* est, uint32_t min_bitrate);
+
+int								rbe_last_estimate(remote_bitrate_estimator_t* est, uint32_t* birate_bps);
+
+void							rbe_incoming_packet(remote_bitrate_estimator_t* est, uint32_t timestamp, int64_t arrival_ts, size_t payload_size);
+
+void							rbe_heartbeat(remote_bitrate_estimator_t* est, int64_t now_ts);
+
+
+#endif
+
+
+
