95b0fde89114c7aafa5bd8bb19a28b45bae2548e
rongxi.yuan <rongxi.yuan@wenba100.com>
2018-04-20 18:00:23 +0800

    增加日志接口

---
+++ cc/razor_log.c
@@ -0,0 +1,22 @@
+#include "razor_log.h"
+#include "razor_api.h"
+#include "cf_platform.h"
+
+static razor_log_func g_log_cb = NULL;
+
+void ex_razor_log(int level, const char* file, int line, const char *fmt, ...)
+{
+	va_list vl;
+	if (g_log_cb == NULL) {
+		va_start(vl, fmt);
+		g_log_cb(level, fmt, vl);
+		va_end(vl);
+	}
+}
+
+void razor_setup_log(razor_log_func log_cb)
+{
+	g_log_cb = log_cb;
+}
+
+
---
+++ cc/razor_log.h
@@ -0,0 +1,16 @@
+#ifndef __razor_log_h_
+#define __razor_log_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "razor_api.h"
+
+void ex_razor_log(int level, const char* file, int line, const char *fmt, ...);
+
+#define razor_debug(...)  				ex_razor_log(0, __FILE__, __LINE__, __VA_ARGS__)
+#define razor_info(...)    				ex_razor_log(1, __FILE__, __LINE__, __VA_ARGS__)
+#define razor_warn(...)					ex_razor_log(2, __FILE__, __LINE__, __VA_ARGS__)
+#define razor_error(...)   				ex_razor_log(3, __FILE__, __LINE__, __VA_ARGS__)
+
+
+#endif
