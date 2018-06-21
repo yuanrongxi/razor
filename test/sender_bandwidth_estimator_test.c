455f61784f16f81a52282148b5ec04194f3b4f39
rongxi.yuan <rongxi.yuan@wenba100.com>
2018-06-21 11:55:43 +0800

    修改单元测试接口

--- test/sender_bandwidth_estimator_test.c
+++ test/sender_bandwidth_estimator_test.c
@@ -19,13 +19,13 @@ static void test_probing(int use_delay_based)
 
 	sender_estimation_set_send_bitrate(est, 200000);
 
-	sender_estimation_update_block(est, 0, 50, 1, now_ms);
+	sender_estimation_update_block(est, 0, 50, 1, now_ms, 200000);
 
 	if (use_delay_based == 0)
-		sender_estimation_update_delay_base(est, now_ms, remb_bps);
+		sender_estimation_update_delay_base(est, now_ms, remb_bps, 0);
 	else
 		sender_estimation_update_remb(est, now_ms, remb_bps);
-	sender_estimation_update(est, now_ms);
+	sender_estimation_update(est, now_ms, 200000);
 
 	rtt = est->last_rtt;
 	bitrate = est->curr_bitrate;
@@ -36,10 +36,10 @@ static void test_probing(int use_delay_based)
 	now_ms += 2001;
 
 	if (use_delay_based == 0)
-		sender_estimation_update_delay_base(est, now_ms, second_remb_bps);
+		sender_estimation_update_delay_base(est, now_ms, second_remb_bps, 0);
 	else
 		sender_estimation_update_remb(est, now_ms, second_remb_bps);
-	sender_estimation_update(est, now_ms);
+	sender_estimation_update(est, now_ms, remb_bps);
 
 	bitrate = 0;
 
@@ -74,9 +74,9 @@ static void bitrate_decrease_without_following_remb()
 	EXPECT_EQ(0, est->last_rtt);
 
 	/*模拟report事件,发生丢包了*/
-	sender_estimation_update_block(est, k_fraction_loss, k_rtt, 100, now_ms);
+	sender_estimation_update_block(est, k_fraction_loss, k_rtt, 100, now_ms, k_initial_bitrate_bps);
 	now_ms += 1000;
-	sender_estimation_update(est, now_ms);
+	sender_estimation_update(est, now_ms, k_initial_bitrate_bps);
 
 	EXPECT_LT(est->curr_bitrate, k_initial_bitrate_bps);
 	EXPECT_EQ(est->last_fraction_loss, k_fraction_loss);
@@ -85,7 +85,7 @@ static void bitrate_decrease_without_following_remb()
 	last_bitrate_bps = est->curr_bitrate;
 
 	now_ms += 1000;
-	sender_estimation_update(est, now_ms);
+	sender_estimation_update(est, now_ms, k_initial_bitrate_bps);
 
 	EXPECT_EQ(est->curr_bitrate, last_bitrate_bps);
 	EXPECT_EQ(est->last_fraction_loss, k_fraction_loss);
