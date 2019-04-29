1d7c1bce45cba2dda33f88a6d7a9a1852b7267b3
rongxi.yuan <rongxi.yuan@wenba100.com>
2019-04-29 19:01:02 +0800

    将帧率调制到24帧每秒

--- sim_test/echo/echoDlg.cpp
+++ sim_test/echo/echoDlg.cpp
@@ -503,7 +503,7 @@ LRESULT CechoDlg::OnConnectSucc(WPARAM wparam, LPARAM lparam)
 
 	video_info_t info;
 	info.pix_format = RGB24;
-	info.rate = 12;
+	info.rate = 24;
 	info.codec = GetCodec();
 	info.width = resolution_infos[i].codec_width;
 	info.height = resolution_infos[i].codec_height;
